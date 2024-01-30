80d3273fa3cd467273603c4beb0edc635e3eb84b
Bookkeeper <yarko12@gmail.com>
2024-01-30 16:12:11 +0200

    Chore: change font-size

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -5,25 +5,25 @@
 </script>
 
 <template>
-    <footer class="flex justify-center border-t border-t-purple-400 px-3 py-6 sm:px-4 sm:py-6">
+    <footer class="flex justify-center border-t border-t-purple-400 px-3 py-4 sm:p-4">
         <div
-            class="container flex flex-col items-center gap-5 text-white sm:grid sm:grid-cols-2 sm:grid-rows-1"
+            class="container flex flex-col items-center gap-y-3 text-white sm:grid sm:grid-cols-2 sm:grid-rows-1"
         >
             <div class="self-start sm:justify-self-start">
-                <h2 class="mb-3 text-lg font-bold sm:text-2xl">Contact me</h2>
+                <h2 class="mb-3 text-lg font-bold sm:text-xl">Contact me</h2>
                 <address class="flex items-center gap-x-2">
                     <i
                         class="fa-regular fa-envelope cursor-pointer text-lg text-purple-300 duration-300 ease-in-out hover:-translate-y-2 hover:text-purple-500 sm:text-2xl"
                     ></i>
                     <a
                         href="mailto:yaroslavmuller98@gmail.com"
-                        class="text-sm duration-300 ease-out hover:opacity-80 sm:text-lg"
+                        class="text-sm duration-300 ease-out hover:opacity-80 sm:text-base"
                         >yaroslavmuller98@gmail.com
                     </a>
                 </address>
             </div>
             <div class="min-w-[250px] self-start sm:justify-self-end">
-                <h2 class="mb-3 text-lg font-bold sm:text-right sm:text-2xl">Media</h2>
+                <h2 class="mb-3 text-lg font-bold sm:text-right sm:text-xl">Media</h2>
                 <ul class="flex gap-x-5 sm:justify-end">
                     <li>
                         <a
@@ -58,7 +58,7 @@
                 </ul>
             </div>
             <div
-                class="col-span-2 justify-self-center text-sm italic max-[300px]:text-center sm:text-base"
+                class="col-span-2 justify-self-center text-xs italic max-[300px]:text-center sm:text-sm"
             >
                 &copy; Copyright {{ currentYear }}. Made by Bookkeeper
             </div>
